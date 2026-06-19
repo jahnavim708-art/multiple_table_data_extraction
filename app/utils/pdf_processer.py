90c3930cdd1a6e8ec5448c6bdbed78fe24a07398
vannadanikhitha <vannadanikhitha@gmail.com>
2026-06-19 13:14:17 +0530

    import the extract_transactionspd file

--- app/utils/pdf_processer.py
+++ app/utils/pdf_processer.py
@@ -1,7 +1,7 @@
 import pdfplumber
 from app.utils.text_cleaner import clean_text
 from app.utils.key_value_extractor import extract_kv_from_line
-
+from app.utils.transaction_extractor import extract_transactions
 
 def process_pdf(pdf_path: str):
     table_data = []
