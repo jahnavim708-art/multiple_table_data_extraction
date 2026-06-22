9fddb2069ef9837afe2361351fd1e1514f9fae14
vannadanikhitha <vannadanikhitha@gmail.com>
2026-06-22 12:28:12 +0530

    changed the one hdfc import file app.utils.file_name to utils.file_name

--- app/utils/pdf_processer.py
+++ app/utils/pdf_processer.py
@@ -1,7 +1,7 @@
 import pdfplumber
 from utils.text_cleaner import clean_text
 from utils.key_value_extractor import extract_kv_from_line
-from utils.transaction_extractor import extract_transactions
+from utils.hdfc_extractor import extract_transactions
 
 def process_pdf(pdf_path: str):
     table_data = []
